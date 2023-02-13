3ef13d63a9e1fbce2f300e4587665706460b73d7
Justin Yi <justinjyi96@gmail.com>
2023-02-13 11:05:29 -0500

    refactor post model

---
+++ server/models/Post.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema({
+  postText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 500,
+      trim: true
+  },
+  postAuthor: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  comments: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment',
+      },
+  ],
+  recipes: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'Recipe',
+      },
+  ]
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
