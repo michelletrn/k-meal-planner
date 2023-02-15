8cc1d5a077454b4ee734e3afa1f97b28078374e9
tommysoprano <thomasjaymaly@gmail.com>
2023-02-15 11:08:45 -0500

    added button to delete items from list and email list to an email

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,10 +3,16 @@ import { Navigate, useParams } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { REMOVE_MEAL } from "../utils/mutations";
+import * as Icon from "react-bootstrap-icons";
 
 const ShoppingList = ({ shoppingList, setShoppingList }) => {
+  const emailBody = shoppingList.join("%0D%0A");
 
-    // const ingredientList = shoppingList.split( "," );
+  const handleDelete = (index) => {
+    setShoppingList((prevList) =>
+      prevList.filter((_, itemIndex) => itemIndex !== index)
+    );
+  };
 
   return (
     <>
@@ -15,11 +21,24 @@ const ShoppingList = ({ shoppingList, setShoppingList }) => {
         <p>Your shopping list is currently empty</p>
       ) : (
         <ul>
-          {shoppingList.map((ingredient) => (
-            <li >{ingredient}</li>
+          {shoppingList.map((ingredient, index) => (
+            <li key={index}>
+              {ingredient}
+              <div
+                className="removeIngredientBtn"
+                onClick={() => handleDelete(index)}
+              >
+                <Icon.Trash style={{ width: "25px", height: "25px" }}>
+                  Remove Ingredient
+                </Icon.Trash>
+              </div>
+            </li>
           ))}
         </ul>
       )}
+      <a href={`mailto:?subject=My Shopping List&body=${emailBody}`}>
+        <button>Send Shopping List via Email</button>
+      </a>
     </>
   );
 };
